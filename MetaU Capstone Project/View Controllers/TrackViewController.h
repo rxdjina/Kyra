4e4552a6f2ba81360f609f761b63b834f2d379f3
rxdjina <55474317+rxdjina@users.noreply.github.com>
2022-08-13 16:03:30 -0700

    Update UI - Display Track

--- MetaU Capstone Project/View Controllers/TrackViewController.h
+++ MetaU Capstone Project/View Controllers/TrackViewController.h
@@ -6,10 +6,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MusicSession.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TrackViewController : UIViewController
+
+@property (nonatomic, strong) MusicSession *session;
+@property (nonatomic, strong) NSDictionary *track;
+
+@property (strong, nonatomic) IBOutlet UIImageView *viewBGImage;
+@property (strong, nonatomic) IBOutlet UIImageView *coverArtImage;
+@property (strong, nonatomic) IBOutlet UILabel *trackNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *artistNameLabel;
+@property (strong, nonatomic) IBOutlet UISlider *trackPlayer;
+@property (strong, nonatomic) IBOutlet UIButton *rewindButton;
+@property (strong, nonatomic) IBOutlet UIButton *playPauseButton;
+@property (strong, nonatomic) IBOutlet UIButton *forwardButton;
+
 
 @end
 
